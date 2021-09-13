a52d49b653ca7e349b7d9f9a7afbcebacfe1a892
Elasepraise <elasepraise@gmail.com>
2021-09-13 22:16:12 +0000

    Updated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
+
+
 def element_at(my_list, idx):
-    ind = len(my_list) - 1
-    if idx < 0:
-        return my_list
-    elif idx > ind:
+    if idx < 0 or idx >= len(my_list):
         return None
-    else:
-        return (my_list[idx])#!/usr/bin/python3
-def element_at(my_list, idx):
-    ind = len(my_list) - 1
-    if idx < 0:
-        return my_list
-    elif idx > ind:
-        return None
-    else:
-        return (my_list[idx])
+    return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
+
+
 def replace_in_list(my_list, idx, element):
-    ind = len(my_list) - 1
-    if idx < 0 or idx > ind:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        my_list[idx] = element
-        new_list = my_list 
-        return new_list#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    ind = len(my_list) - 1
-    if idx < 0 or idx > ind:
-        return my_list
-    else:
-        my_list[idx] = element
-        new_list = my_list 
-        return new_list
+    my_list[idx] = element
+    return my_list
